ad3cd997bc7e733647b0f10da18db7543019eefd
shaheen <shaheen@mail.uni-paderborn.de>
2020-01-21 14:56:31 +0100

    change LICENSE.txt to LICENSE

--- conanfile.py
+++ conanfile.py
@@ -13,7 +13,7 @@ class RDFParser(ConanFile):
     settings = "build_type", "compiler", "os", "arch"
     requires = "TBB/2019_U3@conan/stable", "pegtl/2.8.1@taocpp/stable", "gtest/1.8.1@bincrafters/stable", "fmt/6.0.0@bincrafters/stable", "abseil/20181200@bincrafters/stable"
     generators = "cmake", "cmake_find_package", "cmake_paths"
-    exports = "LICENSE.txt"
+    exports = "LICENSE"
     exports_sources = "include/*", "CMakeLists.txt", "cmake/dummy-config.cmake.in"
     no_copy_source = True
 
